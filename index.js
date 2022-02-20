91d60edca0b8cdf6a4985924c63827880b97cd4d
Katherine Jean-Charles <katherinejc103@gmail.com>
2022-02-20 13:47:03 -0500

    commit tree fda36f3767caf00eb1c7b2fdf8bd8b7dff26d00d

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+//    document.querySelector('#text') .addEventListener('changeText', e)
+   const answer = document.querySelector('#text')
+    answer.innerHTML = 'This is really cool!'
+  });
